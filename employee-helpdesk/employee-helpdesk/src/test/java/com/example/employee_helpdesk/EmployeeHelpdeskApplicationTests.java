5f5a2aa900f40df0a76eba962a4844841b6220c9
KPrasanth34 <KPrasanth@thestackly.com>
2026-08-14 13:48:52 +0530

    Add files via upload

---
+++ employee-helpdesk/employee-helpdesk/src/test/java/com/example/employee_helpdesk/EmployeeHelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employee_helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeHelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
